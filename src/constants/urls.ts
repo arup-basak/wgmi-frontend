15217b4a51fbc1aa19568f495d59aa62ab862a52
Arup Basak <email.arupbasak@gmail.com>
2024-05-28 21:12:52 +0530

    fix url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,9 +1,9 @@
 const urls = {
-  getOdyssey: "/get-odyssey",
-  getStage: "/get-stage",
-  getAllowListBalance: (account: string) => `/allowlist-balance/${account}`,
-  getMintedTxn: (account: string, mintQty: number) => `/get-mint-txn/${account}/${mintQty}`,
-  getPublicListBalance: (account: string) => `/publiclist-balance/${account}`,
+  getOdyssey: "/api/get-odyssey",
+  getStage: "/api/get-stage",
+  getAllowListBalance: (account: string) => `/api/allowlist-balance/${account}`,
+  getMintedTxn: (account: string, mintQty: number) => `/api/get-mint-txn/${account}/${mintQty}`,
+  getPublicListBalance: (account: string) => `/api/publiclist-balance/${account}`,
 };
 
 export default urls;
